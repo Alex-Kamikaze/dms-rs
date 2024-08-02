f003c53671a70b07e370c2b8135f377b00fccb88
XIDY-Dex <sashagameac@gmail.com>
2024-08-02 17:26:44 +0300

    Переведены сообщения об ошибках

--- api/src/errors.rs
+++ api/src/errors.rs
@@ -9,14 +9,14 @@ pub mod errors {
         #[error("Базовый словарь не найден и не предоставлено название языка для поиска другого словаря")]
         /// Ошибка, которая вызывается, если утилита не может найти базового словаря и если при вызове функции не был передан аргумент с названием языка
         BasicDictionaryNotFound,
-        #[error("Cannot parse JSON into dictionary")]
+        #[error("Не удалось спарсить JSON файл словаря")]
         /// Обертка для типа serde_json::Error
         JSONParsingError(#[from] serde_json::Error),
-        #[error("Error occured while translating word")]
+        #[error("Произошла ошибка при переводе слова в API")]
         /// Обертка для типа reqwest::Error
         APIError(#[from] reqwest::Error),
         /// Обертка для типа io::Error
-        #[error("Error occured during working with dictionary file")]
+        #[error("Произошла ошибка при работе с файлом словаря")]
         IOError(#[from] io::Error),
     }
 }
